35b37261f1e067969ff50766dd99edbe36f3928c
MaduraDiasTreinetic <madurachandima6@gmail.com>
2024-09-30 23:04:01 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# sportify
+ Initial setup
---
+++ lib/common/app_colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color black = Color.fromARGB(255, 21, 0, 0);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color ff5050 = Color(0xFFFF5050);
+  static Color gray = Color(0xFFFFFFFF).withOpacity(0.5);
+}
