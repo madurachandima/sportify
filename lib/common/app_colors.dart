92239f00f8e7ce6886227a91a196e70faf71b3a4
MaduraDiasTreinetic <madurachandima6@gmail.com>
2024-10-02 08:57:22 +0530

    update assets

--- lib/common/app_colors.dart
+++ lib/common/app_colors.dart
@@ -4,5 +4,5 @@ class AppColors {
   static const Color black = Color.fromARGB(255, 21, 0, 0);
   static const Color white = Color(0xFFFFFFFF);
   static const Color ff5050 = Color(0xFFFF5050);
-  static Color gray = Color(0xFFFFFFFF).withOpacity(0.5);
+  static Color gray = const Color.fromARGB(50, 0, 0, 0);
 }
--- lib/common/app_images.dart
+++ lib/common/app_images.dart
@@ -1,8 +1,8 @@
 class AppImages {
-  static const _basePath = "lib/assets/images/";
+  static const _basePath = "lib/assets/images";
 
   static const appLogo = "$_basePath/app_logo.svg";
-  static const bgImage = "$_basePath/app_logo.svg";
+  static const bgImage = "$_basePath/bg_image.png";
   static const overlayImage = "$_basePath/bg_overlay_image.svg";
   static const mainBgImage = "$_basePath/main_bg_logo.svg";
 }
